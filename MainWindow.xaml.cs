78b187e94afb0c3538fc3c0f0e5322056f1bf280
RandomName <odpowelson@gmail.com>
2023-03-01 20:44:44 -0700

    Added Timeline slider and dark theme

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Media;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Windows.Threading;
 
 namespace MusicPlayer
 {
@@ -34,16 +36,40 @@ namespace MusicPlayer
         public MainWindow()
         {
             player = new MediaPlayer();
+            player.MediaOpened += Player_MediaOpened;
             player.MediaEnded += Player_MediaEnded;
             InitializeComponent();
             RefreshSongs();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += timer1_Tick;
+            timer.Interval = TimeSpan.FromSeconds(0.1);
+            timer.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSlider.Value = player.Position.TotalMilliseconds;
+        }
+
+        private void SeekPosition(object? sender, EventArgs e)
+        {
+            player.Position = TimeSpan.FromMilliseconds((int)TimeSlider.Value);
+        }
+
+        private void Player_MediaOpened(object? sender, EventArgs e)
+        {
+            TimeSlider.Maximum = player.NaturalDuration.TimeSpan.TotalMilliseconds;
         }
 
         private void Player_MediaEnded(object? sender, EventArgs e)
         {
+            state = MusicState.NONE;
+            player.Stop();
             SongList.SelectedIndex += 1;
             if (SongList.SelectedIndex >= SongList.Items.Count)
                 SongList.SelectedIndex = 0;
+            PlayPause();
+
         }
 
         private void AddSong(string name, string filename)
@@ -73,9 +99,9 @@ namespace MusicPlayer
             }
         }
 
-        private void PlayPause(object sender, RoutedEventArgs e)
+        public void PlayPause()
         {
-            if(SongList.SelectedIndex < 0) return;
+            if (SongList.SelectedIndex < 0) return;
             switch (state)
             {
                 case MusicState.NONE:
@@ -92,6 +118,11 @@ namespace MusicPlayer
                     player.Play();
                     break;
             }
+        }
+
+        private void PlayPause(object sender, RoutedEventArgs e)
+        {
+            PlayPause();
         }
 
         private void ChangeVolume(object sender, RoutedPropertyChangedEventArgs<double> e)
